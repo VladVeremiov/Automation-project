fb9dd773242fd9262284fde7f61b62b7bd033ed8
Vlad <vladveremiov@gmail.com>
2026-09-02 14:16:05 +0300

    Add unique email validation

--- tests/tests_user.py
+++ tests/tests_user.py
@@ -81,3 +81,14 @@ def test_user_with_data(api):
     assert "id" in data
     assert "name" in data
     assert "email" in data
+
+def test_users_with_unique_email(api):
+    emails = []
+    response = api.get_users()
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) == 10
+    for user in data:
+        emails.append(user["email"])
+    assert len(emails) == len(set(emails))
+
